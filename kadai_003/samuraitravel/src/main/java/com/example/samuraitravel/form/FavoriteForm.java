ea574578edde23622866a90c721787afa7ccacf6
rimando0827 <kasaharashojirou@gmail.com>
2025-02-21 23:41:46 +0900

    first commit

---
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/form/FavoriteForm.java
@@ -0,0 +1,12 @@
+package com.example.samuraitravel.form;
+
+import lombok.Data;
+
+@Data
+public class FavoriteForm {
+	
+private int houseId;
+	
+	private int userId;
+
+}
---
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/repository/ReviewRepository.java
@@ -0,0 +1,18 @@
+package com.example.samuraitravel.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.samuraitravel.entity.Review;
+import com.example.samuraitravel.entity.User;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+	public Page<Review> findByHouseIdOrderByCreatedAtDesc(Integer houseId, Pageable pageable );
+	public Page<Review> findByHouseOrderByCreatedAtDesc(User review, Pageable pageable );
+	
+	List<Review> findByHouseIdOrderByCreatedAtDesc(Integer houseId);
+
+}
