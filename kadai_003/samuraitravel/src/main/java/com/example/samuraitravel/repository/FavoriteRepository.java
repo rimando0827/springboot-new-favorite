64dbd29d8a48b460a42a38da5a1866786456c5ff
rimando0827 <kasaharashojirou@gmail.com>
2025-03-02 16:29:33 +0900

    first commit

--- kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/repository/FavoriteRepository.java
+++ kadai_003/samuraitravel/src/main/java/com/example/samuraitravel/repository/FavoriteRepository.java
@@ -1,5 +1,11 @@
 package com.example.samuraitravel.repository;
 
+
+
+
+
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,7 +14,7 @@ import com.example.samuraitravel.entity.Favorite;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer>{
 	public Page<Favorite> findByUserIdOrderByCreatedAtDesc(Integer useId, Pageable pageable );
-	
+	Optional<Favorite> findByHouseIdAndUserId(Integer houseId, Integer userId);
 		
 	}
 
